b044afb1913066dfad041440c397fb2c6c1cc672
DELFINA IARA KALTMAN <48794948@ort>
2025-05-09 13:53:48 -0300

    controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,5 +19,23 @@ public class HomeController : Controller
         ViewBag.discos = Info.listaDiscos;
         return View();
     }
-    
+
+    public IActionResult SelectDisco(int id)
+    {
+        //Consigna: Debe recibir el dni de un alumno que desea ver. Luego traer la info de los integrantes del diccionario y buscar el dni recibido.
+        /*Cargar la información requerida del integrante correspondiente en un ViewBag.
+        Cargar el DNI en otro ViewBag
+        Retornar la vista infoDatosPersonales.
+        */
+        Info.InicializarGrupo();
+        if (Info.listaDiscos.ContainsKey(id))
+        {
+            ViewBag.disco = Info.listaDiscos[id].disco;
+            ViewBag.DNI = DNI;
+        }
+       
+        return View ("InfoDatosPersonales");
+
+    }
+
 }
